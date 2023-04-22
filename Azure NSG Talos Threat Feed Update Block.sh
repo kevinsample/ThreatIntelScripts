86af61b1820cadcfc5486933601ce45ad43946e9
Kevin Sample <kevinasample@gmail.com>
2023-04-22 10:27:50 -0500

    Update Azure NSG Talos Threat Feed Update Block.sh

--- Azure NSG Talos Threat Feed Update Block.sh
+++ Azure NSG Talos Threat Feed Update Block.sh
@@ -5,6 +5,6 @@ wget -O blocklist.txt https://www.talosintelligence.com/documents/ip-blacklist
 #you can test the variable using "echo $blocklist"
 blocklist=`cat blocklist.txt`
 
-#Finally update an existing NSG outbound rule with a new list of destination IP addresses
+#Update an existing NSG outbound rule with a new list of destination IP addresses
 az network nsg rule update --direction Outbound --name TalosThreatFeed-Block --nsg-name ServerSubnetIn --resource-group Azure_Site_Recovery --destination-address-prefixes $blocklist
 
